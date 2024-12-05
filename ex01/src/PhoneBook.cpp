336ed5a097fd74806b55cee84052ae2a43149663
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-12-05 12:32:19 +0900

    mk08 ex01 err_add_main

--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -6,40 +6,38 @@
 /*   By: hosokawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 14:02:59 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/11/27 15:36:41 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/12/04 17:04:00 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
 #include "Contacts.hpp"
+#include "PhoneBook.hpp"
 #include "main.hpp"
 
-
-PhoneBook::PhoneBook():add_count(0)
-{}
-
+PhoneBook::PhoneBook() : add_count(0)
+{
+}
 
 void PhoneBook::deleat_old_info()
 {
-	int i;
+	int	i;
 
-	i=1;
-
-	while(i<8)
+	i = 1;
+	while (i < 8)
 	{
-		list[i-1].set_firstname(list[i].get_firstname());
-		list[i-1].set_lastname(list[i].get_lastname());
-		list[i-1].set_nickname(list[i].get_nickname());
-		list[i-1].set_number(list[i].get_number());
-		list[i-1].set_secret(list[i].get_secret());
+		list[i - 1].set_firstname(list[i].get_firstname());
+		list[i - 1].set_lastname(list[i].get_lastname());
+		list[i - 1].set_nickname(list[i].get_nickname());
+		list[i - 1].set_number(list[i].get_number());
+		list[i - 1].set_secret(list[i].get_secret());
 		i++;
 	}
 }
 
 void PhoneBook::add()
 {
-	print_add_format();	
-	if(add_count>7)
+	print_add_format();
+	if (add_count > 7)
 	{
 		deleat_old_info();
 		list[7].add_info();
@@ -49,38 +47,37 @@ void PhoneBook::add()
 	add_count++;
 }
 
-
 void PhoneBook::search()
 {
-	std::string index;
+	int	index;
 
+	std::string index_str;
 	print_search_format(list);
-
-	std::cout<<"-whose infomation do you want to know ?-\n";
-	std::cout<<"                 *enter the index number of a person.\n";
-	std::cout<<std::endl;
-
-	std::cout<<"---------\n";
-	std::cout<<">>index:";
-	std::getline(std::cin,index);
-	std::cout<<"---------.\n";
-
-	//std::stringstream ss(index);
-
-	//i_index=index.atoi();
-	std::cout<<"\n";
-	list[0].print_info();
-
-	std::cout<<"------------------------------.";
-	std::cout<<std::endl;
+	std::cout << "-whose infomation do you want to know ?-\n";
+	std::cout << "                 *enter the index number of a person.\n";
+	std::cout << std::endl;
+	std::cout << "---------\n";
+	std::cout << ">>index:";
+	std::getline(std::cin, index_str);
+	std::cout << "---------.\n";
+	index = std::stoi(index_str);
+	if (index < 1 || 8 < index)
+	{
+		std::cout << "please 1~8 number" << std::endl;
+		return ;
+	}
+	if (list[index - 1].get_firstname() == "no data")
+	{
+		std::cout << "No contact found at this index." << std::endl;
+		return ;
+	}
+	std::cout << "\n";
+	list[index-1].print_info();
+	std::cout << "------------------------------.";
+	std::cout << std::endl;
 }
 
 void PhoneBook::exitPhoneBook()
 {
 	exit(EXIT_SUCCESS);
 }
-
-
-
-
-
